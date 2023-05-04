d1753f861aedb395185bf4b733fcdfffb7c370c2
Kamil Akhmetzyanov <akhmetzyanov.kamil.00@mail.ru>
2023-05-04 14:36:21 +0300

    чето чето

---
+++ RecommendationsApp/GameInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecommendationsApp
+{
+    public partial class GameInfo : Form
+    {
+        public GameInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MainForm mainForm= new MainForm();
+            mainForm.ShowDialog();
+        }
+    }
+}
